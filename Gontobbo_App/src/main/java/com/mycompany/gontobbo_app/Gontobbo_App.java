5543fa979e622362326afd510c0e811879ef2470
Rahat Talukder <rahatulab@gmail.com>
2024-12-22 03:11:39 +0600

    commiting First Release of gontobbo frontend

---
+++ Gontobbo_App/src/main/java/com/mycompany/gontobbo_app/Gontobbo_App.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gontobbo_app;
+
+/**
+ *
+ * @author User
+ */
+public class Gontobbo_App {
+
+    public static void main(String[] args) {
+        HomePage newHome = new HomePage();
+        newHome.setVisible(true);
+//        
+//        ReceptionistDboard recepDboard = new ReceptionistDboard();
+//        recepDboard.setVisible(true);
+//        
+        
+        
+        
+    }
+}
---
+++ Gontobbo_App/src/main/java/com/mycompany/gontobbo_app/Login.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gontobbo_app;
+
+/**
+ *
+ * @author User
+ */
+public class Login {
+    
+}
