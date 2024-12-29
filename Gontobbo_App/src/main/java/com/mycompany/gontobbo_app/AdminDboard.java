fa88cbb4c57871b2aaecd2479d5ad121340ad2a8
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-12-30 04:59:30 +0600

    Update timeStampConverter function call

--- Gontobbo_App/src/main/java/com/mycompany/gontobbo_app/AdminDboard.java
+++ Gontobbo_App/src/main/java/com/mycompany/gontobbo_app/AdminDboard.java
@@ -319,25 +319,6 @@ public void showLineChart(){
         
     }//GEN-LAST:event_onAddAdminDboard
 
-    public static String timeStampConverter(String timestampStr) {
-        // Input timestamp in milliseconds
-        
-        long timestamp = Long.parseLong(timestampStr);
-
-        // Convert timestamp to LocalDateTime
-        LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
-
-        // Define the output formatter
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        // Format the LocalDateTime to the desired output format
-        String formattedDateTime = dateTime.format(formatter);
-
-        // Print the formatted date-time string
-        System.out.println("Formatted Date-Time: " + formattedDateTime);
-        return formattedDateTime;
-    }
-
     public void renderTableData() {
         System.out.println("AdminDboard");
         DBConnection db = new DBConnection();
@@ -354,7 +335,7 @@ public void showLineChart(){
         ((javax.swing.table.DefaultTableModel) jTable1.getModel()).setRowCount(0);
 
         for(Map<String, String> trip : trips) {
-            String dateTime = timeStampConverter(trip.get("start_time"));
+            String dateTime = DBConnection.timeStampConverter(trip.get("start_time"));
             trip.put("start_time", dateTime);
             String[] row = {
                 trip.get("id"),
